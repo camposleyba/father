4d19c15c5103daef802a6551f497b78ac02f0db9
camposleyba <martcamposleyba@gmail.com>
2021-09-16 09:22:02 -0300

    more changes

--- metric_display/display/views.py
+++ metric_display/display/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .forms import *
+from .models import metric
 
 def home(request):
 	if request.method =="POST":
 		inputF = inputf(request.POST)
 	else:
 		inputF = inputf()
-	context = {'form':inputF
+	context = {
+		'form':inputF
 
 	}
 	return render(request, 'display/home.html', context)
