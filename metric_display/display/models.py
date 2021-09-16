e8a6f0a2c151063eacdcc4b340e4c0f1d316ca8e
camposleyba <martcamposleyba@gmail.com>
2021-09-16 10:19:24 -0300

    changes to table

--- metric_display/display/models.py
+++ metric_display/display/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class metric(models.Model):
 	manager = models.CharField(blank=True, max_length=200)
 	developer = models.CharField(blank=True, max_length=200)
+	bot_nbr = models.CharField(blank=True, max_length=200)
 	tot_bots = models.IntegerField(default=0)
 	tot_hours = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
