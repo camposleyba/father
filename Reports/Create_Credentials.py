a0357d313138ea1fb6efebf9e5ba2d1142a59dfc
camposleyba <martcamposleyba@gmail.com>
2022-06-06 14:33:21 -0300

    changes to create_credentials

--- Reports/Create_Credentials.py
+++ Reports/Create_Credentials.py
@@ -9,13 +9,21 @@ class createCredentials():
 	def create(self, name):
 		''' Create function, takes 1 parameter which is the name of the credentials file to be created
 		and locally saved.
-		It uses pickle library to create a binary file with the user and password provided in the
+			It uses pickle library to create a binary file with the user and password provided in the
 		class initiation.'''
+		self.name = name
 		dict_cred={
 			"user":self.user,
 			"password":self.psw
 		}
 
-		with open(name, "wb") as f:
+		with open(self.name, "wb") as f:
 			pickle.dump(dict_cred,f)
 			print("Credentials successfully created!")
+
+	def load(self):
+		''' Load function, is used to check the previously saved credentials on the file already provided on 
+		the create function'''
+		with open(self.name, "rb") as f:
+			file = pickle.load(f)
+		return "Creadentials stored are: \n user: {} \n password: {}".format(file['user'],file['password'])
