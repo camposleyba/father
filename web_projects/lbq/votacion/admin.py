2452e5cfcad241db6fffad92f9360dc134f0a5f2
camposleyba <martcamposleyba@gmail.com>
2023-11-01 14:52:23 -0300

    cleaned

--- web_projects/lbq/votacion/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import votados, categorias
-
-
-class votadosAdmin(admin.ModelAdmin):
-	list_display = ('nombre','categoria','cuentavotos')
-
-admin.site.register(votados, votadosAdmin)
-admin.site.register(categorias)
