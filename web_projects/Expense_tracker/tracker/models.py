0af92fb980ddc7bfe51e7576309942a226895602
camposleyba <martcamposleyba@gmail.com>
2024-01-04 11:27:24 -0300

    updated repo

--- web_projects/Expense_tracker/tracker/models.py
+++ web_projects/Expense_tracker/tracker/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class Expense(models.Model):
-    
+
     quarter = models.CharField(max_length=2)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
-    monto = models.DecimalField(max_digits=7, decimal_places=2, default=00000.00)
+    monto = models.DecimalField(max_digits=10, decimal_places=2, default=00000000.00)
     rendido = models.BooleanField(default=False)
-    
+
     def __str__(self):
     	return self.title
 
 class qBudget(models.Model):
 	quarter = models.CharField(max_length=2)
-	budget = models.DecimalField(max_digits=7, decimal_places=2, default=00000.00)
-	tc = models.DecimalField(max_digits=7, decimal_places=2, default=00000.00)
+	budget = models.DecimalField(max_digits=10, decimal_places=2, default=00000000.00)
+	tc = models.DecimalField(max_digits=10, decimal_places=2, default=00000000.00)
 
 	def __str__(self):
 		return self.quarter
